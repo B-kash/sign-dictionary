de0fb8a0cba8389b6219d6c616f9fcfdee250eb4
B-kash <bikashchapagain047@gmail.com>
2024-12-09 10:54:33 +0100

    ♻️ fetch new items cleaned up

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,33 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:sign_dictionary/models/grid_item.model.dart';
+import 'package:sign_dictionary/services/category_fetcher.dart';
 import 'package:sign_dictionary/widgets/infinite_scroll_grid.dart';
 
 class HomeScreen extends StatefulWidget {
+
   @override
   State<StatefulWidget> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<GridItem> items = List.generate(
-      100, (index) => GridItem(name: 'Item $index'),
-      growable: true);
-      // TODO: replace this with actual api call may be
-  Future<void> fetchMoreItems() async {
-    await Future.delayed(Duration(seconds: 2));
-    var newItems = List.generate(
-        100, (index) => GridItem(name: 'Item ${items.length + index + 1}')
-        );
-    setState(() {
-      items.addAll(newItems);
-    });
-    print('Fetched more items');
-
-  }
+    final CategoryFetcherService _categoryFetcherService = CategoryFetcherService();
 
   @override
   Widget build(BuildContext context) {
     return Center(
         child:
-            InfiniteScrollGrid(items: items, fetchMoreItems: fetchMoreItems));
+            InfiniteScrollGrid(categoryFetcher: _categoryFetcherService,));
   }
 }
--- lib/widgets/infinite_scroll_grid.dart
+++ lib/widgets/infinite_scroll_grid.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:sign_dictionary/models/grid_item.model.dart';
+import 'package:sign_dictionary/services/category_fetcher.dart';
 
-import '../models/grid_item.model.dart';
+
 
 class InfiniteScrollGrid extends StatefulWidget {
-  final List<GridItem> items; // The list of items to display
-  final Future<void> Function() fetchMoreItems; // Function to fetch more items
+  final CategoryFetcherService categoryFetcher; // Service to fetch items
 
-  const InfiniteScrollGrid({super.key, required this.items, required this.fetchMoreItems});
+  const InfiniteScrollGrid({super.key, required this.categoryFetcher});
 
   @override
   _InfiniteScrollGridState createState() => _InfiniteScrollGridState();
@@ -14,49 +15,85 @@ class InfiniteScrollGrid extends StatefulWidget {
 
 class _InfiniteScrollGridState extends State<InfiniteScrollGrid> {
   final ScrollController _scrollController = ScrollController();
+  List<GridItem> _items = []; // List to store fetched items
+  bool _isFetching = false; // Track if fetch is in progress
 
   @override
   void initState() {
     super.initState();
+    _fetchInitialItems();
 
-    // Detect when user scrolls to the bottom to load more items
+    // Listen for scroll events to detect end of the list
     _scrollController.addListener(() {
-      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
-        // Trigger the function to fetch more items when scrolled to the bottom
-        widget.fetchMoreItems();
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent &&
+          !_isFetching) {
+        _fetchMoreItems();
       }
+    });
+  }
+
+  Future<void> _fetchInitialItems() async {
+    setState(() {
+      _isFetching = true;
+    });
+
+    final items = await widget.categoryFetcher.fetchMoreItems(_items.length);
+    setState(() {
+      _items = items;
+      _isFetching = false;
+    });
+  }
+
+  Future<void> _fetchMoreItems() async {
+    setState(() {
+      _isFetching = true;
+    });
+
+    final newItems = await widget.categoryFetcher.fetchMoreItems(_items.length);
+    setState(() {
+      _items.addAll(newItems);
+      _isFetching = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      controller: _scrollController,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 4, // Number of items per row
-        crossAxisSpacing: 0, // Space between columns
-        mainAxisSpacing: 0, // Space between rows
-      ),
-      itemCount: widget.items.length, // Total number of items
-      itemBuilder: (context, index) {
-        // Get the current item
-        final item = widget.items[index];
-        return Card(
-          elevation: 5,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // Conditionally display the logo if it's not null
-              item.logo != null
-                  ? Image.network(item.logo!, height: 50, width: 50) // Display logo if available
-                  : SizedBox(height: 50, width: 50), // Placeholder if no logo
-              SizedBox(height: 10),
-              Text(item.name, style: TextStyle(fontSize: 16)), // Display name
-            ],
+    return Stack(
+      children: [
+        GridView.builder(
+          controller: _scrollController,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
           ),
-        );
-      },
+          itemCount: _items.length,
+          itemBuilder: (context, index) {
+            final item = _items[index];
+            return Card(
+              elevation: 5,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  item.logo != null
+                      ? Image.network(item.logo!, height: 50, width: 50)
+                      : SizedBox(height: 50, width: 50),
+                  const SizedBox(height: 10),
+                  Text(item.name, style: const TextStyle(fontSize: 16)),
+                ],
+              ),
+            );
+          },
+        ),
+        if (_isFetching)
+          Positioned(
+            bottom: 20,
+            left: 0,
+            right: 0,
+            child: Center(child: CircularProgressIndicator()),
+          ),
+      ],
     );
   }
 }
